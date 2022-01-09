627ee329d57845b7ff2e70409c1b6b68f0359806
Acktarad <acktarad@gmail.com>
2022-01-09 11:04:05 +0300

    Приступил к созданию Web приложения

---
+++ src/main/java/com/example/testtaskitsevice/TestTaskItSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtaskitsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskItSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskItSeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtaskitsevice/TestTaskItSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskitsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskItSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
